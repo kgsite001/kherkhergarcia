97eae047ba6cc0aebaaf0efb15cf26e48215a3ab
Katze033 <josh.katzenmeyer@gmail.com>
2020-06-08 15:10:53 -0500

    HTML/CSS for hero menu. Copy for client stories.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,25 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import couple from '../images/Couple.png'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="main-flex_container">
-      <section>
-        <h1>We are with you all the way.</h1>
-        <h2>Our reputation is built on our commitment to service and a strong record of success</h2>
+      <section className="hero-menu_container">
+        <span role="img" aria-label="Couplle speaking with an attorney."></span>
+        <div className="hero-menu_copy">
+          <h1>We are with you all the way.</h1>
+          <h2>Our reputation is built on our commitment to service and a strong record of success</h2>
+          <button>Ask Us Anything ➝</button>
+        </div>
       </section>
-      <section></section>
+      <section className="testimonials_container">
+        <h1>The moment you are injured, the clock starts. The longer you wait, the harder your case gets.</h1>
+        <p>Ruby's day started like any other. We approach our clients like family. Our team is dedicated to putting your needs
+          first and helping you achieve the best possible result for you. Our focus is your justice.</p>
+      </section>
       <section></section>
       <section></section>
       <section></section>
