7dd86d1798237d0a85a29346cad29bf97f65271c
Katze033 <josh.katzenmeyer@gmail.com>
2020-06-10 11:32:01 -0500

    Continued styling for About Us and Review sections. Next: Results tables and google review excerpts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
         </div>
       </section>
       <section className="testimonials_container">
-      <div className="testimonials_media">
+        <div className="testimonials_media">
           <p>Video here</p>
         </div>
         <div className="testimonials_copy">
@@ -27,7 +27,7 @@ const IndexPage = () => (
           first and helping you achieve the best possible result for you. Our focus is your justice.</p>
           <button>See Ruby's Story ➝</button>
         </div>
-        
+
       </section>
       <section className="results_container">
         <h1 className="section_title">Results</h1>
@@ -40,8 +40,42 @@ const IndexPage = () => (
         </div>
         <button>See More Victories ➝</button>
       </section>
-      <section></section>
-      <section></section>
+      <section className="about_container">
+        <div className="about-main_container">
+          <div className="about-main_copy">
+            <h1 className="section_title">About Us</h1>
+            <h2 className="section_subtitle">We are relentless in our effort to seek justice for our clients."</h2>
+            <p className="section_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <div className="about-main_media">
+            <p>Video</p>
+          </div>
+        </div>
+        <div className="about-sub_container">
+          <div className="about-sub_copy">
+            <h1>Experienced Trial Lawyers</h1>
+            <p>Our experience is great. We havve worked on cases aroudn the country for years and we're prepared to get you through this.</p>
+          </div>
+          <div className="about-sub_copy">
+            <h1>Service Comes First</h1>
+            <p>We are focused first on our clients</p>
+          </div>
+          <div className="about-sub_copy">
+            <h1>Ask Us Anything</h1>
+            <p>We want you to make the best decision for you and your family Call one of our lawyers for a free consultation and get the answers you need to choose the right law firm.</p>
+          </div>
+        </div>
+      </section>
+      <section className="reviews_container">
+        <h1 className="section_title">Reviews</h1>
+        <h2 className="section_subtitle">What our clients say about us:</h2>
+        <p className="section_paragraph">Better service means better recovery for our clients.</p>
+        <div className="reviews_google">
+          <p>Review</p>
+          <p>Review</p>
+        </div>
+        <button>Read More Reviews ➝</button>
+      </section>
       <section></section>
       <section></section>
       <section></section>
