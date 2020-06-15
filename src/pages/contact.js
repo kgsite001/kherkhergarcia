c5e51608d5e9c4297f11dd17e25c9feb584305e3
Katze033 <josh.katzenmeyer@gmail.com>
2020-06-15 17:27:18 -0500

    Contact page creation and styling.

---
+++ src/pages/contact.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+    <Layout>
+        <SEO title="Home" />
+        <div className="main-flex_container">
+
+            <section className="contact-top_container flex-layout">
+                <div className="contact-top_copy">
+                    <h1 className="section_subtitle">We are ready to help you right now.</h1>
+                    <p className="section_paragraph">Contact us even if you aren't ready to hire someone and just need some basic questions answered. Ask us anything.</p>
+                </div>
+                <div className="contact-top_form">
+                    <p>Insert form here.</p>
+                </div>
+            </section>
+            <section className="contact-bottom_container flex-layout">
+                <div className="contact-bottom_copy">
+                    <h1 className="section_subtitle">Kherkher Garcia LLP</h1>
+                    <address>
+                        <h1 className="section_paragraph">Houston Office</h1>
+                        <p>801 Travis Street, Suite 2175</p>
+                        <p>Houston, Texas 77002</p>
+                        <br></br>
+                        <p>Phone: <a>(713) 333-1030</a></p>
+                        <p>Fax: (713) 333-1029</p>
+                        <a>contactus@kherkhergarcia.com</a>
+                    </address>
+                </div>
+                <div className="contact-bottom_map">
+                    <p>Map here</p>
+                </div>
+            </section>
+        </div>
+    </Layout>
+)
+
+export default ContactPage
