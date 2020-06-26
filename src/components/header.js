9c43251cf93cf5ee8d1d8deababc515cbfc95c0d
Katze033 <josh.katzenmeyer@gmail.com>
2020-06-26 14:26:54 -0500

    Desktop nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,34 @@ import React from "react"
 
 import '../styles/styles.css'
 
-const Header = ({ siteTitle }) => (
+import Logo from '../images/logo_top.png'
+import Home from '../images/home-solid.svg'
+
+const Header = () => (
   <header>
-    <nav>
-      <ul className="nav-flex_container">
-        <li>Logo</li>
-        <li>About Us</li>
-        <li>What We Do</li>
-        <li>Results</li>
-        <li>What Our Clients Say</li>
-        <li>What You Need to Know</li>
-        <li>Contact Us</li>
-        <li>Espanol</li>
-        <li>Button</li>
-      </ul>
+    <nav role="navigation">
+      <div id="menuToggle">
+        <input type="checkbox"></input>
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul className="nav-flex_container" id="menu">
+          <img className="icon_large" src={Logo}></img>
+          <img className="icon_small" src={Home}></img>
+          <li>About Us</li>
+          <li>What We Do</li>
+          <li>Results</li>
+          <li>What Our Clients Say</li>
+          <li>What You Need to Know</li>
+          <li>Contact Us</li>
+          <li>Espanol</li>
+          <button className="call_button">
+            <p>Call or Text 24/7:</p>
+            <address>(888) 123-4567</address>
+            <p>Ayuda en Espanol</p>
+          </button>
+        </ul>
+      </div>
     </nav>
   </header>
 )
