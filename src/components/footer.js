3c6458ebe8a06867a6097a05556702017ad5a751
Katze033 <josh.katzenmeyer@gmail.com>
2020-07-02 14:47:18 -0500

    Adjust margins and modify footer size.

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ import React from "react"
 const Footer = () => (
     <footer className="padding-small">
         <div>
-            <p className="paragraph-small">The information on this website is for general information purposes only. Nothing on this site should be taken as legal advice for any individual case or situation. This information is not intended to create, and receipt or viewing does not constitute, an attorney-client relationship.</p>
-            <p className="paragraph-small">For a breakdown of the fees and expenses associated with the verdicts and settlements listed on our site, please visit our Results page.</p>
+            <p className="paragraph-smaller">The information on this website is for general information purposes only. Nothing on this site should be taken as legal advice for any individual case or situation. This information is not intended to create, and receipt or viewing does not constitute, an attorney-client relationship.</p>
+            <p className="paragraph-smaller">For a breakdown of the fees and expenses associated with the verdicts and settlements listed on our site, please visit our Results page.</p>
         </div>
     </footer>
 )
