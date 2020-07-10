a4550e8908246d81a8332625b3aaa10d32aab6fe
Katze033 <josh.katzenmeyer@gmail.com>
2020-07-10 09:42:50 -0500

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,54 @@ import React from "react"
 
 
 const Footer = () => (
-    <footer className="padding-small">
-        <div>
-            <p className="paragraph-smaller">The information on this website is for general information purposes only. Nothing on this site should be taken as legal advice for any individual case or situation. This information is not intended to create, and receipt or viewing does not constitute, an attorney-client relationship.</p>
-            <p className="paragraph-smaller">For a breakdown of the fees and expenses associated with the verdicts and settlements listed on our site, please visit our Results page.</p>
-        </div>
+    <footer>
+        <section className="ft-main">
+            <div class="ft-main-item">
+                <h2 className="ft-logo">Kherkher Garcia LLP</h2>
+                <ul>
+                    <li><a href="#">Site Map</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Disclaimer</a></li>
+                </ul>
+            </div>
+
+            <div className="ft-main-item ft-office">
+                <h2 className="ft-title">Principal Office</h2>
+                <ul>
+                    <li>Kherkher Garcia LLP</li>
+                    <li> 2925 Richmond Ave., Suite 1560</li>
+                    <li>Houston, Texas  77098 </li>
+                    <li><a className="ft-button ft-directions" href="#">Get Directions</a></li>
+                </ul>
+            </div>
+            <div className="ft-main-item ft-contact">
+                <h2 className="ft-title">Contact Us</h2>
+                <ul>
+                    <li ><a className="ft-button" href="#">(800) 123-4567</a></li>
+                    <li><a className="ft-button ft-text" href="#">Text Us</a></li>
+                    <li><a className="ft-button ft-email" href="#">Email Us</a></li>
+                </ul>
+            </div>
+            <div className="ft-main-item">
+                <h2 className="ft-title">Follow Us</h2>
+                <ul className="ft-social-list">
+                    <li><a className="fa-facebook" href="#">Facebook</a></li>
+                    <li><a className="fa-twitter" href="#">Twitter</a></li>
+                    <li><a className="fa-instagram" href="#">Instagram</a></li>
+                    <li><a className="fa-youtube" href="#">YouTube</a></li>
+                    <li><a className="fa-linkedin" href="#">LinkedIn</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <section className="ft-copyright">
+            <p>&copy; Copyright Kherkher Garcia LLP Personal Injury Trial Lawyers. All Rights Reserved.</p>
+        </section>
+
+        <section className="ft-legal">
+            <p>The information presented on this website is not intended to be taken as legal advice for any case or situation. Additionally, nothing shown or mentioned on this website constitutes nor creates an attorney-client relationship.</p>
+        </section>
+
     </footer>
 )
 
